faaaa2f7c96f31f6f68042b1e8626483399ac92e
Keisuke-Toku-2000 <keisuke20000923@gmail.com>
2022-01-10 16:05:38 +0900

    ktor入門

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,10 +1,17 @@
 package com.jetbrains.handson.httpapi
 
 import io.ktor.application.Application
+import io.ktor.features.*
+import io.ktor.application.*
+import io.ktor.serialization.*
+import routes.registerCustomerRoute
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-
+    install(ContentNegotiation){
+        json()
+    }
+    registerCustomerRoute()
 }
